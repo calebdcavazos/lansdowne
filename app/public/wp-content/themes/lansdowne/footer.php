bd646c951a83940bfc4e9437c8347ffd2959b318
Caleb <calebdcavazos@gmail.com>
2019-03-26 07:41:08 -0400

    adjusted so that all vendor files are in one location

--- app/public/wp-content/themes/lansdowne/footer.php
+++ app/public/wp-content/themes/lansdowne/footer.php
@@ -1,7 +1,7 @@
     <!-- Footer -->
     <footer class="bg-black small text-center text-white-50">
       <div class="container">
-        Copyright &copy; Lansdowne Neighborhood Civic League 2018
+        Copyright &copy; Lansdowne Neighborhood Civic League 2019
       </div>
     </footer>
 
--- app/public/wp-content/themes/lansdowne/functions.php
+++ app/public/wp-content/themes/lansdowne/functions.php
@@ -1,9 +1,7 @@
 <?php 
 
 	function lansdowne_files() {
-		wp_enqueue_script('jquery', get_theme_file_uri('vendor/jquery/jquery.min.js'), NULL, '1.0', true);
-		wp_enqueue_script('bootstrap-js', get_theme_file_uri('vendor/bootstrap/js/bootstrap.bundle.min.js'), NULL, '1.0', true);
-		wp_enqueue_script('easing', get_theme_file_uri('vendor/jquery-easing/jquery.easing.min.js'), NULL, '1.0', true);
+		wp_enqueue_script('vendor', get_theme_file_uri('js/vendor.js'), NULL, '1.0', true);
 		wp_enqueue_script('grayscale', get_theme_file_uri('js/grayscale.min.js'), NULL, '1.0', true);
 		wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
 		wp_enqueue_style('bootstrap-core', '//stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css');
